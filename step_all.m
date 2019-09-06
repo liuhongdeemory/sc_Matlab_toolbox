75ccd1a1f27b51715696109e5cba3b521c697f24
liuhongdeemory <liuhongde@seu.edu.cn>
2019-09-06 12:27:15 +0800

    Update step_all.m

--- step_all.m
+++ step_all.m
@@ -1,5 +1,4 @@
-clear;
-clc; close all;
+clear all; clc; close all;
 disp('load data.')
 load ('matrix_scRNAE_GEOD_83139.mat');
 % load ('matrix_scRNA.mat');
@@ -14,7 +13,8 @@ M=X;
 
 disp('filtering genes.')
 cut=0.4;
-[Sort_M Sort_gene]=sort_gene_by_percent(M,gene,cut); % you can slip this function
+[Sort_M Sort_gene]=sort_gene_by_percent(M,gene,cut); % you can skip this function
+% if the step is skipped, the inputted data for "my_Find_HVGs.m" should be "M" and "gene".
 
 disp('Find highly variable genes.')
 figure1=figure('color','w');
@@ -27,6 +27,7 @@ isplot=1;
 dis_type={'chebychev';'cosine';'euclidean';'mahalanobis'};
 Perplexity=40; % para
 Y = my_tSNE(X_HVGs',dis_type{2},Perplexity);
+% if you would not like to use martix of highly variable genes, you can use M to replace X_HVGs.
 
 
 disp('Identify subgourps for tSNE (clustering).')
@@ -34,6 +35,7 @@ figure1=figure('color','w');
 cluster_num=[];isplot=1;
 opt_type=[1:4];%opt_type_full={'genlouvain';'louvain';'kmeans';'linkage'};
 [S Q]=my_Cluster_algorithm(Y(:,1),Y(:,2),opt_type(1),cluster_num,isplot);
+% S is community (cluster) ID for each cell.
 disp(strcat('Q=',num2str(Q)));
 disp(strcat('Num of clusters=',num2str(length(unique(S)))));
 
@@ -41,9 +43,3 @@ disp('identifying differentially expressed genes and write them in to files');
 meth_type=1;isplot=1;
 figure1=figure('color','w');
 [DG]=my_Identify_diff_gene_one2others(X_HVGs,S,HVGs,meth_type,isplot);
-
-
-
-
-
-
